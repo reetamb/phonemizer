5820e943e8a2353bb216906e00119b6e9d67c81e
reetamb <57857847+reetamb@users.noreply.github.com>
2025-01-18 14:16:07 -0800

    added frozen flask

--- app.py
+++ app.py
@@ -7,11 +7,5 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-if __name__ == "__main__":
-    import os
-    HOST = os.environ.get("SERVER_HOST", "localhost")
-    try:
-        PORT = int(os.environ.get("SERVER_PORT", "5555"))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+if __name__ == '__main__':
+    app.run(debug=True)
